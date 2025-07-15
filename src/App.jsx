17632b7123932f94f491192fdec5ca419255c0a0
Francesco De Vivo <francescodvf@yahoo.it>
2025-07-15 08:44:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header.jsx';
+import Quiz from './components/Quiz.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Quiz />
+    </div>
+  );
+}
+
+export default App;
